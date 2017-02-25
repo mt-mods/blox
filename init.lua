c4dd45ba7f47d05fcc54a1c0233e76ef5db006dd
Vanessa Ezekowitz <vanessaezekowitz@gmail.com>
2017-02-25 00:58:25 -0500

    set "etx" meta flag if converting old nodes, too

--- init.lua
+++ init.lua
@@ -573,6 +573,7 @@ minetest.register_lbm({
 		minetest.set_node(pos, { name = "blox:"..material.."_"..pattern, param2 = paletteidx })
 		local meta = minetest.get_meta(pos)
 		meta:set_string("dye", "unifieddyes:"..color)
+		meta:set_string("palette", "ext")
 	end
 })
 
